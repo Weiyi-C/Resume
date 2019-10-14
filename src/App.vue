95b45855d032b96b0912450439366be3335f9afd
郑维一 <weiyi@zhengweiyi.cn>
2019-10-14 14:22:17 +0800

    修改 应用入口
    添加 navbar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,76 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div>
+    <div class="nav">
+      <nav>
+        <div>
+          <ul>
+            <li v-for='item in nav' :key='item'>{{ item.name }}</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      nav: [
+        { name: '返回首页', url: '' },
+        { name: '基本信息', url: '' },
+        { name: 'Web', url: '' },
+        { name: '移动', url: '' },
+        { name: '设计', url: '' }
+      ]
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  line-height: 2rem;
+}
+
+body {
+  margin: 0;
+  font-size: 16px;
+  font-family: "Microsoft YaHei";
+  background-color: #FEFEFE;
+}
+
+.nav {
+  position: flxed;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  overflow: hidden;
+  background-color: #FFF;
+  box-shadow: 0px -2px 20px 3px rgba(0,0,0,0.2);
+}
+
+.nav nav {
+  margin: 0 auto;
+  width: 1180px
+}
+
+.nav ul {
+  float: right;
+  list-style-type: none;
+}
+
+.nav ul li {
+  float: left;
+  padding: 0 2rem;
+  line-height: 5;
+}
+
+.row {
+  padding: 20px 0;
 }
 </style>
