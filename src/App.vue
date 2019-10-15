a4719f4b62c8c4881477d9818ce2322639690f71
郑维一 <weiyi@zhengweiyi.cn>
2019-10-15 13:58:49 +0800

    创建 Navbar和基本信息页面组件
    创建 Navbar.vue导航栏组件
    创建 Basic.vue基本信息组件
    修改 App.vue入口
    修改 router以接入新组件
    修改 首页title
    添加 静态资源文件

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
   <div>
-    <div class="nav">
-      <nav>
-        <div>
-          <ul>
-            <li v-for='item in nav' :key='item'>{{ item.name }}</li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+    <Navbar />
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
 export default {
   name: 'App',
   data () {
@@ -26,51 +19,13 @@ export default {
         { name: '设计', url: '' }
       ]
     }
+  },
+  components: {
+    Navbar
   }
 }
 </script>
 
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  line-height: 2rem;
-}
 
-body {
-  margin: 0;
-  font-size: 16px;
-  font-family: "Microsoft YaHei";
-  background-color: #FEFEFE;
-}
-
-.nav {
-  position: flxed;
-  top: 0px;
-  left: 0px;
-  width: 100%;
-  overflow: hidden;
-  background-color: #FFF;
-  box-shadow: 0px -2px 20px 3px rgba(0,0,0,0.2);
-}
-
-.nav nav {
-  margin: 0 auto;
-  width: 1180px
-}
-
-.nav ul {
-  float: right;
-  list-style-type: none;
-}
-
-.nav ul li {
-  float: left;
-  padding: 0 2rem;
-  line-height: 5;
-}
-
-.row {
-  padding: 20px 0;
-}
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-navbar toggleabale="lg" type="dark" variant="success">
+      <b-container>
+        <b-navbar-brand href="#">
+          <b-img :src=logoSrc fluid alt="郑维一 logo">郑维一 logo</b-img>
+          郑维一
+        </b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template slot="button-content"><em>User</em></template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      meal: '',
+      logoSrc: require('../assets/logo.svg')
+    }
+  },
+  methods: {
+    getMeal () {
+    }
+  }
+}
+</script>
